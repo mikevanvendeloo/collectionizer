7c30d6fb3ac148edbc61cf7e32be8ebefbc073d1
Mike van Vendeloo <mike@jpoint.nl>
2017-02-24 13:22:22 +0100

    Initial version generated

---
+++ src/main/java/net/vanvendeloo/collections/CollectionizerApplication.java
@@ -0,0 +1,12 @@
+package net.vanvendeloo.collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionizerApplication.class, args);
+	}
+}
